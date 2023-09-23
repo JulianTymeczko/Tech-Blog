d1ac647ed6f053dab7da31a57bca7b929cc2f3bd
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-23 11:39:17 -0400

    fixed user.js

--- models/User.js
+++ models/User.js
@@ -31,13 +31,3 @@ User.init(
 );
 
 module.exports = User;
-const User = require("./User");
-const Blog = require("./Blog");
-
-User.hasMany(Blog, { foreignKey: "user_id" });
-Blog.belongsToMany(User);
-
-module.exports = {
-  User,
-  Blog,
-};
