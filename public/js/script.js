37de7055f8b643a90aa5405fb360cbf4fe6d2237
Julian Houle-T <jhouletymeczko@gmail.com>
2023-09-29 18:32:54 -0400

    fixing bugs with the backend

--- public/js/script.js
+++ public/js/script.js
@@ -32,19 +32,17 @@ document.getElementById("sign-up")?.addEventListener("click", (event) => {
   });
 });
 
-// document.getElementById("log-out")?.addEventListener("click", (event) => {
-//   event.preventDefault();
-
-//   fetch("/api/users/Log-out", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       username: document.getElementById("floatingInputSign").value,
-//       password: document.getElementById("floatingPasswordSign").value,
-//     }),
-//   }).then((res) => {
-//     console.log(res);
-//   });
-// });
+document.getElementById("modal-button")?.addEventListener("click", () => {
+  fetch("/api/blogs/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: document.getElementById("modal-input").value,
+      paragraph: document.getElementById("exampleFormControlTextarea1").value,
+    }),
+  }).then((res) => {
+    console.log(res);
+  });
+});
